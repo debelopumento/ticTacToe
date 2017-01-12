cecc1b9c310ee36d6f3e8340368dfca651e614a9
Di Ye <imdiye@gmail.com>
2017-01-12 13:20:12 -0800

    added function to prevent placing pawn in an ocupied unit

--- app.js
+++ app.js
@@ -9,15 +9,11 @@ $(function(){
 	initGame();
 });
 
-
-
 function restart() {
 	event.preventDefault();
-	console.log(1);
 	$('.js-boardDiv').html();
 	initGame();
 }
-
 
 function initGame() {
 	for (i=1; i <= boardSize; i++) {
@@ -34,7 +30,6 @@ function renderBoard(){
 	var boardHtml = '';
 	for (i=1; i <= boardSize; i++) {
 		boardHtml += '<div class="boardRow" data-row="' + i + '">';
-		
 		for (j=1; j <= boardSize; j++) {
 			boardHtml += '<span class="unit empty js-unit" data-column="' + j + '">' + '</span>';
 		}
@@ -49,47 +44,50 @@ function renderBoard(){
 function playGame() {
 	$('.js-unit').click(function(event){
 		console.log(this);
-		var coordinate = $(this).text();
-		//var currentRow = coordinate[0];
 		var currentRow = $(this).closest('div').attr('data-row');
 		var currentColumn = $(this).closest('span').attr('data-column');
-		if (XsTurn===true) {
-			$(event.currentTarget).toggleClass('X');
-			board.row[currentRow][currentColumn] = "X";
-		}
-		else {
-			$(event.currentTarget).toggleClass('O');
-			board.row[currentRow][currentColumn] = "O";
-		}
-		console.log("currentRow", currentRow, "currentColumn", currentColumn);
-		
-		//check if game is finished
-		var gameOver = true;
-			//check row
-			for (j=1; j <=boardSize; j++) {
-				if (board.row[currentRow][currentColumn] != board.row[currentRow][j]) {gameOver = false;}
+		//check if this unit is empty
+		if (board.row[currentRow][currentColumn]==="unknown") {
+			if (XsTurn===true) {
+				$(event.currentTarget).toggleClass('X');
+				board.row[currentRow][currentColumn] = "X";
 			}
-			if (gameOver===true) {alertFinish();}
-			//check column
+			else {
+				$(event.currentTarget).toggleClass('O');
+				board.row[currentRow][currentColumn] = "O";
+			}
+			console.log("currentRow", currentRow, "currentColumn", currentColumn);
+			
+			//check if game is finished
 			var gameOver = true;
-			for (i=1; i <=boardSize; i++) {
-				if (board.row[currentRow][currentColumn] != board.row[i][currentColumn]) {gameOver = false;}
-			}
-			if (gameOver===true) {alertFinish();}
-			//check diagonal
-			if(boardSize===3) {
-				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn]) {alertFinish();}
-				if (board.row[1][3]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][1]===board.row[currentRow][currentColumn]) {alertFinish();}
-			}
-			if (boardSize===4) {
-				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn]) {alertFinish();}
-				if (board.row[1][4]===board.row[currentRow][currentColumn] && board.row[2][3]===board.row[currentRow][currentColumn] && board.row[3][2]===board.row[currentRow][currentColumn] && board.row[4][1]===board.row[currentRow][currentColumn]) {alertFinish();}
-			}
-			if (boardSize===5) {
-				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn] && board.row[5][5]===board.row[currentRow][currentColumn]) {alertFinish();}
-				if (board.row[1][5]===board.row[currentRow][currentColumn] && board.row[2][4]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][2]===board.row[currentRow][currentColumn] && board.row[5][1]===board.row[currentRow][currentColumn]) {alertFinish();}
-			}
-		XsTurn = !XsTurn;
+				//check row
+				for (j=1; j <=boardSize; j++) {
+					if (board.row[currentRow][currentColumn] != board.row[currentRow][j]) {gameOver = false;}
+				}
+				if (gameOver===true) {alertFinish();}
+				//check column
+				var gameOver = true;
+				for (i=1; i <=boardSize; i++) {
+					if (board.row[currentRow][currentColumn] != board.row[i][currentColumn]) {gameOver = false;}
+				}
+				if (gameOver===true) {alertFinish();}
+				//check diagonal
+				if(boardSize===3) {
+					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn]) {alertFinish();}
+					if (board.row[1][3]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][1]===board.row[currentRow][currentColumn]) {alertFinish();}
+				}
+				if (boardSize===4) {
+					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn]) {alertFinish();}
+					if (board.row[1][4]===board.row[currentRow][currentColumn] && board.row[2][3]===board.row[currentRow][currentColumn] && board.row[3][2]===board.row[currentRow][currentColumn] && board.row[4][1]===board.row[currentRow][currentColumn]) {alertFinish();}
+				}
+				if (boardSize===5) {
+					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn] && board.row[5][5]===board.row[currentRow][currentColumn]) {alertFinish();}
+					if (board.row[1][5]===board.row[currentRow][currentColumn] && board.row[2][4]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][2]===board.row[currentRow][currentColumn] && board.row[5][1]===board.row[currentRow][currentColumn]) {alertFinish();}
+				}
+			XsTurn = !XsTurn;
+		}
+
+		
 	});	
 
 	$('.js-newgame').click(function(){
