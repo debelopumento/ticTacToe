46853d67c5af45ba4d3147d36ade9a4edd2a451c
Di Ye <imdiye@gmail.com>
2017-01-13 00:34:40 -0800

    alert draw when game finishes

--- app.js
+++ app.js
@@ -43,10 +43,15 @@ function renderBoard(){
 
 
 function playGame() {
+	var turn=1;
+
 	$('.js-unit').click(function(event){
 		console.log(this);
 		var currentRow = $(this).closest('div').attr('data-row');
 		var currentColumn = $(this).closest('span').attr('data-column');
+		
+
+
 		//check if this unit is empty
 		if (board.row[currentRow][currentColumn]==="unknown") {
 			if (XsTurn===true) {
@@ -59,33 +64,41 @@ function playGame() {
 			}
 			console.log("currentRow", currentRow, "currentColumn", currentColumn);
 			
+
 			//check if game is finished
 			var gameOver = true;
-				//check row
-				for (j=1; j <=boardSize; j++) {
-					if (board.row[currentRow][currentColumn] != board.row[currentRow][j]) {gameOver = false;}
-				}
-				if (gameOver===true) {alertFinish();}
-				//check column
-				var gameOver = true;
-				for (i=1; i <=boardSize; i++) {
-					if (board.row[currentRow][currentColumn] != board.row[i][currentColumn]) {gameOver = false;}
-				}
-				if (gameOver===true) {alertFinish();}
-				//check diagonal
-				if(boardSize===3) {
-					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn]) {alertFinish();}
-					if (board.row[1][3]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][1]===board.row[currentRow][currentColumn]) {alertFinish();}
-				}
-				if (boardSize===4) {
-					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn]) {alertFinish();}
-					if (board.row[1][4]===board.row[currentRow][currentColumn] && board.row[2][3]===board.row[currentRow][currentColumn] && board.row[3][2]===board.row[currentRow][currentColumn] && board.row[4][1]===board.row[currentRow][currentColumn]) {alertFinish();}
-				}
-				if (boardSize===5) {
-					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn] && board.row[5][5]===board.row[currentRow][currentColumn]) {alertFinish();}
-					if (board.row[1][5]===board.row[currentRow][currentColumn] && board.row[2][4]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][2]===board.row[currentRow][currentColumn] && board.row[5][1]===board.row[currentRow][currentColumn]) {alertFinish();}
-				}
+			//check row
+			for (j=1; j <=boardSize; j++) {
+				if (board.row[currentRow][currentColumn] != board.row[currentRow][j]) {gameOver = false;}
+			}
+			if (gameOver===true) {alertFinish();}
+			//check column
+			var gameOver = true;
+			for (i=1; i <=boardSize; i++) {
+				if (board.row[currentRow][currentColumn] != board.row[i][currentColumn]) {gameOver = false;}
+
+			}
+			if (gameOver===true) {alertFinish();}
+			//check diagonal
+			if(boardSize===3) {
+				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn]) {alertFinish();}
+				if (board.row[1][3]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][1]===board.row[currentRow][currentColumn]) {alertFinish();}
+			}
+
+			if (boardSize===4) {
+				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn]) {alertFinish();}
+				if (board.row[1][4]===board.row[currentRow][currentColumn] && board.row[2][3]===board.row[currentRow][currentColumn] && board.row[3][2]===board.row[currentRow][currentColumn] && board.row[4][1]===board.row[currentRow][currentColumn]) {alertFinish();}
+			}
+			if (boardSize===5) {
+				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn] && board.row[5][5]===board.row[currentRow][currentColumn]) {alertFinish();}
+				if (board.row[1][5]===board.row[currentRow][currentColumn] && board.row[2][4]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][2]===board.row[currentRow][currentColumn] && board.row[5][1]===board.row[currentRow][currentColumn]) {alertFinish();}
+			}
+
+			if (turn === boardSize * boardSize) {
+				alertDraw();
+			}
 			XsTurn = !XsTurn;
+			turn = turn + 1;
 		}
 
 		
@@ -103,7 +116,7 @@ function playGame() {
 
 }
 
-function alertFinish(){
+function alertFinish() {
 	if (XsTurn===true) {
 		var winner = "X";
 	}
@@ -118,4 +131,15 @@ function alertFinish(){
 		restart();
 	});
 }	
-	
+
+function alertDraw() {
+	var endalert = '';
+	endalert += '<div id="dialog" title="Game Over">';
+	endalert += '<p>Draw!</p>';
+	endalert += '<button class="js-newgame">New Game</button></div>'
+	$(".message").html(endalert);
+	$( "#dialog" ).dialog();
+	$('.js-newgame').click(function(){
+		restart();
+	});
+}
