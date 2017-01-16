7dbc4337566fbf1a4367aab9e4ec26fdef814cf7
Di Ye <imdiye@gmail.com>
2017-01-16 12:41:04 -0800

    fiexed game-never-finishes bug

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ function renderBoard(boardSize, board){
 		boardHtml += '</div>';
 	}
 	$('.js-board').html(boardHtml);
+	
 	playGame(boardSize, board);
 }
 
@@ -54,62 +55,71 @@ function renderBoard(boardSize, board){
 function playGame(boardSize, board) {
 	var turn=1;
 	var XsTurn = true;
+	var gameFinished = false;
 	$('.js-unit').click(function(event){
 		console.log(this);
 		var currentRow = $(this).closest('div').attr('data-row');
 		var currentColumn = $(this).closest('span').attr('data-column');
 		
+		//check if game has finished
+		if (gameFinished === false) {
 
+			//check if this unit is empty
+			if (board.row[currentRow][currentColumn]==="unknown") {
+				if (XsTurn===true) {
+					$(event.currentTarget).toggleClass('X');
+					board.row[currentRow][currentColumn] = "X";
+				}
+				else {
+					$(event.currentTarget).toggleClass('O');
+					board.row[currentRow][currentColumn] = "O";
+				}
+				console.log("currentRow", currentRow, "currentColumn", currentColumn);
+				
 
-		//check if this unit is empty
-		if (board.row[currentRow][currentColumn]==="unknown") {
-			if (XsTurn===true) {
-				$(event.currentTarget).toggleClass('X');
-				board.row[currentRow][currentColumn] = "X";
-			}
-			else {
-				$(event.currentTarget).toggleClass('O');
-				board.row[currentRow][currentColumn] = "O";
-			}
-			console.log("currentRow", currentRow, "currentColumn", currentColumn);
-			
+				//check if game is finished
+				var gameOver = true;
+				//check row
+				for (j=1; j <=boardSize; j++) {
+					if (board.row[currentRow][currentColumn] != board.row[currentRow][j]) {gameOver = false;}
+				}
+				if (gameOver===true) {
+					gameFinished = true;
+					alertFinish(XsTurn, boardSize);
+				}
+				//check column
+				var gameOver = true;
+				for (i=1; i <=boardSize; i++) {
+					if (board.row[currentRow][currentColumn] != board.row[i][currentColumn]) {gameOver = false;}
 
-			//check if game is finished
-			var gameOver = true;
-			//check row
-			for (j=1; j <=boardSize; j++) {
-				if (board.row[currentRow][currentColumn] != board.row[currentRow][j]) {gameOver = false;}
-			}
-			if (gameOver===true) {alertFinish(XsTurn, boardSize);}
-			//check column
-			var gameOver = true;
-			for (i=1; i <=boardSize; i++) {
-				if (board.row[currentRow][currentColumn] != board.row[i][currentColumn]) {gameOver = false;}
+				}
+				if (gameOver===true) {
+					gameFinished = true;
+					alertFinish(XsTurn, boardSize);
+				}
+				//check diagonal
+				if(boardSize===3) {
+					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize); gameFinished = true;}
+					if (board.row[1][3]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][1]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize); gameFinished = true;}
+				}
 
-			}
-			if (gameOver===true) {alertFinish(XsTurn, boardSize);}
-			//check diagonal
-			if(boardSize===3) {
-				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize);}
-				if (board.row[1][3]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][1]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize);}
-			}
+				if (boardSize===4) {
+					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize); gameFinished = true;}
+					if (board.row[1][4]===board.row[currentRow][currentColumn] && board.row[2][3]===board.row[currentRow][currentColumn] && board.row[3][2]===board.row[currentRow][currentColumn] && board.row[4][1]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize); gameFinished = true;}
+				}
+				if (boardSize===5) {
+					if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn] && board.row[5][5]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize); gameFinished = true;}
+					if (board.row[1][5]===board.row[currentRow][currentColumn] && board.row[2][4]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][2]===board.row[currentRow][currentColumn] && board.row[5][1]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize); gameFinished = true;}
+				}
 
-			if (boardSize===4) {
-				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize);}
-				if (board.row[1][4]===board.row[currentRow][currentColumn] && board.row[2][3]===board.row[currentRow][currentColumn] && board.row[3][2]===board.row[currentRow][currentColumn] && board.row[4][1]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize);}
+				if (turn === boardSize * boardSize) {
+					gameFinished = true;
+					alertDraw(boardSize);
+				}
+				XsTurn = !XsTurn;
+				turn = turn + 1;
 			}
-			if (boardSize===5) {
-				if (board.row[1][1]===board.row[currentRow][currentColumn] && board.row[2][2]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][4]===board.row[currentRow][currentColumn] && board.row[5][5]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize);}
-				if (board.row[1][5]===board.row[currentRow][currentColumn] && board.row[2][4]===board.row[currentRow][currentColumn] && board.row[3][3]===board.row[currentRow][currentColumn] && board.row[4][2]===board.row[currentRow][currentColumn] && board.row[5][1]===board.row[currentRow][currentColumn]) {alertFinish(XsTurn, boardSize);}
-			}
-
-			if (turn === boardSize * boardSize) {
-				alertDraw(boardSize);
-			}
-			XsTurn = !XsTurn;
-			turn = turn + 1;
 		}
-
 		
 	});	
 
@@ -139,6 +149,7 @@ function alertFinish(XsTurn, boardSize) {
 	$('.js-newgame').click(function(){
 		restart(boardSize);
 	});
+	//restart(boardSize);
 }	
 
 function alertDraw(boardSize) {
@@ -151,4 +162,5 @@ function alertDraw(boardSize) {
 	$('.js-newgame').click(function(){
 		restart(boardSize);
 	});
+	//restart(boardSize);
 }
